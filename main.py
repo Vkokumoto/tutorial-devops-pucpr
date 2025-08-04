b0044acf445804442d51bd945ef884bb4c8ae208
Victor Okumoto <okumotovictor@gmail.com>
2025-08-04 14:00:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ async def root():
     return {"message": "Hello World"}
 
 
-# http://127.0.0.1:8000/teste
-@app.get("/teste")
+# http://127.0.0.1:8000/teste1
+@app.get("/teste1")
 async def funcaoteste():
     return {"teste": True, "num_aleatorio": random.randint(0, 1000)}
