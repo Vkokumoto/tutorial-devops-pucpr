31b8edf7b827aa05a403dbe8277336fd1a4c3c70
Victor Okumoto <okumotovictor@gmail.com>
2025-08-11 15:54:30 -0300

    tentando fazer rodar plmds

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""Módulo principal da aplicação FastAPI."""
 import random
 
 from fastapi import FastAPI
@@ -8,10 +9,12 @@ app = FastAPI()
 # http://127.0.0.1:8000/helloworld
 @app.get("/helloworld")
 async def root():
+    """Executa a lógica X."""
     return {"message": "Hello World"}
 
 
 # http://127.0.0.1:8000/funcaoteste
 @app.get("/funcaoteste")
 async def funcaoteste():
+    """Executa a lógica X."""
     return {"teste": True, "num_aleatorio": random.randint(0, 1000)}
