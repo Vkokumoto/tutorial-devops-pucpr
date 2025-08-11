66cdcb96cf1ac550d7440ca77a5e81a892e89d40
Victor Okumoto <okumotovictor@gmail.com>
2025-08-11 16:50:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ app = FastAPI()
 # http://127.0.0.1:8000/helloworld
 @app.get("/helloworld")
 async def root():
-    # Endpoint que retorna a mensagem 'Hello World'.
+    '''Endpoint que retorna a mensagem 'Hello World'.'''
     return {"message": "Hello World"}
 
 
 # http://127.0.0.1:8000/funcaoteste
 @app.get("/funcaoteste")
 async def funcaoteste():
-    # Endpoint de teste que retorna um número aleatório e um valor booleano.
+    '''Endpoint de teste que retorna um número aleatório e um valor booleano.'''
     return {"teste": True, "num_aleatorio": random.randint(0, 1000)}
