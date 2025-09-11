231feb2606f9ec5fad9c426eeb0415714c01c8c7
Victor Okumoto <okumotovictor@gmail.com>
2025-09-11 13:50:24 -0300

    Testes

--- test/test.py
+++ test/test.py
@@ -14,7 +14,7 @@ def funcaoteste():
 
 def test_create_estudante():
     estudante_teste = Estudante(name="Fulano", curso="ADS", ativo=False)
-    assert estudante_teste == create_estudante()
+    assert estudante_teste == create_estudante(estudante_teste)
 
 
 def test_update_estudante_negativo():
